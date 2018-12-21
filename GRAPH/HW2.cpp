eb6cab1a7b4d016f27fbacc2b5ef641bfc2dfb08
Ekaterina Morozova <31797019+leserpenteau@users.noreply.github.com>
2018-12-21 19:53:13 +0300

    Update HW2.cpp

--- GRAPH/HW2.cpp
+++ GRAPH/HW2.cpp
@@ -1,5 +1,32 @@
 /* Реализовать топологическую сортировку */
 
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+/*int n; // число вершин
+vector<int> g[MAXN]; // граф
+bool used[MAXN];
+vector<int> ans;
+ 
+void dfs (int v) {
+	used[v] = true;
+	for (size_t i=0; i<g[v].size(); ++i) {
+		int to = g[v][i];
+		if (!used[to])
+			dfs (to);
+	}
+	ans.push_back (v);
+}
+ 
+void topological_sort() {
+	for (int i=0; i<n; ++i)
+		used[i] = false;
+	ans.clear();
+	for (int i=0; i<n; ++i)
+		if (!used[i])
+			dfs (i);
+	reverse (ans.begin(), ans.end());
+	*/
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 #include<iostream>
 #include<vector>
 #include<utility>
